01e71b723f00b705bd557d52ea61f9d32b7df54b
Diego Sanchez <diego.sanchez@kadevjo.com>
2018-11-08 21:45:59 -0600

    commit

---
+++ app/src/main/java/sv/dk/com/youbetterwrite/AgregarSeccion.java
@@ -0,0 +1,13 @@
+package sv.dk.com.youbetterwrite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgregarSeccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_seccion);
+    }
+}
